4ecdb370bc67a16aa4f54babf60cb96ebe16ae79
Nagykéri Bence <nuttyka@gmail.com>
2017-01-30 20:29:33 +0100

    Rectangle svg added.

--- src/directives/script/sorting-rectangles.directive.js
+++ src/directives/script/sorting-rectangles.directive.js
@@ -8,7 +8,7 @@
         return {
             bindToController: true,
             scope: {
-                values: '&'
+                values: '='
             },
             controller,
             controllerAs: 'sortingRectangles',
@@ -19,8 +19,32 @@
 
     function controller($log) {
         const vm = this;
-        vm.svgWidth = 555;
-        vm.svgHeight = 666;
+        vm.svgWidth = 800;
+        vm.svgHeight = 800;
+        vm.rectanglesWidth = 50;
+        vm.numberOfRectangles = vm.values.length;
+        vm.getXPosition = getXPosition;
+        vm.getRectHeight = getRectHeight;
 
+        function getXPosition(index) {
+            return (vm.svgWidth/(vm.numberOfRectangles + 1) * (index + 1)) - vm.rectanglesWidth/2;
+        }
+
+        function getRectHeight(randomizedValue) {
+            vm.minRandomNum = 0;
+            vm.maxRandomNum = 10;
+
+            let newMin = 0;
+            let newMax = 100;
+
+            let randomizedRange = (vm.maxRandomNum - vm.minRandomNum);
+
+            let displayedRange = (newMax - newMin);
+            let newValue = (((randomizedValue - vm.minRandomNum) * displayedRange) / randomizedRange) + newMin;
+            if (newValue === 0){
+                newValue = 1;
+            }
+            return newValue;
+        }
     }
 }());
